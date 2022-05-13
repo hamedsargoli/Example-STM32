7bed662ad570c0d2891e0d1e7697854d340bb0c3
Hamed Sargoli <41745585+hamedsargoli@users.noreply.github.com>
2022-05-13 21:38:42 +0430

    add EX-4

---
+++ Clock/EX4 - HAL_RCC_GetSysClockFreq/MDK-ARM/RTE/_clock_4/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'clock 4' 
+ * Target:  'clock 4' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
