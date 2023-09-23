54bea0aa2e77240300a8760e5fce731dd10cf5f4
Hamed Sargoli <41745585+hamedsargoli@users.noreply.github.com>
2023-09-23 12:43:37 +0330

    add gif action
    2023-09-23

--- Timer/Basic/Register/8/main.c
+++ Timer/Basic/Register/8/main.c
@@ -1,5 +1,5 @@
 /*****************************************************************
-- Example 8: Increase the time using the OPM feature in timer 7 ( 4 secend for blinking led)
+- Example 8: Increase the time using the OPM feature in timer 7 ( 5 secend for blinking led)
 					  Intrupt Enable Timer 6 and 7
 					* One Pulse Mode ACtive for Timer 7
 						Freg(CK_CNT) = 250 Hz  for Timer 6 and 7 
